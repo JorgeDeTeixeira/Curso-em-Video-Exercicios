a5af0e826ff1baf543dd3f858cca5b6c6edac8fd
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-05-08 14:22:21 -0300

    Exércicio 4 - Mundo 1

---
+++ Mundo 1/ex005.py
@@ -0,0 +1,4 @@
+#Exercício Python 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+num = int(input("Digite um número:"))
+print(f"Sucessor de {num} é {num + 1}.")
+print(f"Antecessor de {num} é {num - 1}.")
