633e80b49458aeb510384983f51e7c3ec777fdde
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-05-08 14:08:19 -0300

    Exércicio 3 - Mundo 1

---
+++ Mundo 1/ex003.py
@@ -0,0 +1,5 @@
+#Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input("Digite o 1° número:"))
+n2 = int(input("Digite o 2° número:"))
+
+print(f"A soma entre {n1} e {n2} é igual a:{n1+n2}")
