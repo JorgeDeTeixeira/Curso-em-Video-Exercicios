78dcc1f9fc812538a62142dcbe0d1a8acce6e0db
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-05-08 20:11:55 -0300

    Exércicio 27 - Mundo 1

---
+++ Mundo 1/ex027.py
@@ -0,0 +1,5 @@
+# Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+nome = str(input("Digite seu nome completo:")).strip()
+n = nome.split()
+print(f"Primeiro nome:{n[0]}")
+print(f"Último nome:{n[-1]}")
