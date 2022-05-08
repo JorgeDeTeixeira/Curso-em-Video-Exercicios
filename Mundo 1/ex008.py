0a224fcfc9b876a22b0519451d3d91db8c954495
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-05-08 14:40:22 -0300

    Exércicio 7 - Mundo 1

---
+++ Mundo 1/ex008.py
@@ -0,0 +1,8 @@
+#Exercício Python 8: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+m = float(input("Uma distância em metros:"))
+print(f"{m / 1000}km")
+print(f"{m / 100}hm")
+print(f"{m / 10}dam")
+print(f"{m * 10}dm")
+print(f"{m * 100}cm")
+print(f"{m * 1000}mm")
