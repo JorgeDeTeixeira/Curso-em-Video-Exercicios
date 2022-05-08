ae3d3c7f0f0c97edeabb8673aaf78044d9f282da
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2022-05-08 14:27:17 -0300

    Exércicio 6 - Mundo 1

---
+++ Mundo 1/ex006.py
@@ -0,0 +1,5 @@
+#Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+num = int(input("Digite um número:"))
+print(f"Dobro de {num}:{num * 2}")
+print(f"Triplo de {num}:{num * 3}")
+print(f"Raiz quadrada de {num}:{num ** (1/2)}")
